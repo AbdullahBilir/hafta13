935d61d7921454a08a62f7449fc8140a06393395
Abdullah <abdullahblrr@gmail.com>
2024-04-15 16:27:16 +0300

    todolist

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, toggleComplete } from "../redux/todoSlice";
 
 const TodoItem = ({ id, title, completed }) => {
-	return (
-		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
-			<div className='d-flex justify-content-between'>
-				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
-					{title}
-				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
-		</li>
-	);
+  const vekilFonksiyon = useDispatch();
+
+  function handleClick() {
+    vekilFonksiyon(toggleComplete({ id, completed: !completed }));
+  }
+
+  function deleteItem() {
+    vekilFonksiyon(deleteTodo({ id }));
+  }
+
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center">
+          <label>
+            <input
+              type="checkbox"
+              onClick={handleClick}
+              className="mr-3"
+              checked={completed}
+            />
+          </label>
+          {title}
+        </span>
+        <button onClick={deleteItem} className="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+  const totalComplete = useSelector((state) =>
+    state.todos.filter((todo) => {
+      return todo.completed === true;
+    })
+  );
+
+  return <h4 className="mt-3">Total Complete Items: {totalComplete.length}</h4>;
 };
 
 export default TotalCompleteItems;
